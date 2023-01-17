50e6b942944a03544ef4edb699c4c07148423055
kadirnar <kadir.nar@hotmail.com>
2023-01-17 14:35:06 +0300

    Added pip support.

---
+++ setup.py
@@ -0,0 +1,54 @@
+import io
+import os
+import re
+
+import setuptools
+
+
+def get_long_description():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    with io.open(os.path.join(base_dir, "README.md"), encoding="utf-8") as f:
+        return f.read()
+
+
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def get_version():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    version_file = os.path.join(current_dir, "trackerhub", "__init__.py")
+    with io.open(version_file, encoding="utf-8") as f:
+        return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
+
+
+setuptools.setup(
+    name="trackerhub",
+    version=get_version(),
+    author="kadirnar",
+    license="MIT",
+    description="Real-time Multi-Object Tracking Library",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kadirnar/trackerhub",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=get_requirements(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="deep-learning, vision, object-tracker, object-detection, yolov7, detector, yolov5, tracker,strongsort,bytetack,ocsort,norfair",
+)
